a35b4db689914510e08b44552c87f8683865ca77
Никита Кремнёв <nkremnev3003@gmail.com>
2023-10-10 20:55:56 +0300

    Исправления

--- zadanie2/zadanie2.cpp
+++ zadanie2/zadanie2.cpp
@@ -4,87 +4,39 @@
 #include <cmath>
 #include <iomanip>
 
-// Реализация Линейного конгруэнтного метода
-class LinearCongruentialGenerator {
+class RandomNumberGenerator {
 private:
     uint32_t seed;
-    const uint32_t a = 1664525;
-    const uint32_t c = 1013904223;
-    const uint32_t m = 0xFFFFFFFF;
+    const uint32_t multiplier = 1664525;
+    const uint32_t increment = 1013904223;
+    const uint32_t modulus = 0xFFFFFFFF;
 
 public:
-    LinearCongruentialGenerator(uint32_t initialSeed) : seed(initialSeed) {}
+    RandomNumberGenerator(uint32_t initialSeed) : seed(initialSeed) {}
 
-    // Генерация случайного числа
     uint32_t generate() {
-        seed = (a * seed + c) % m;
+        seed = (multiplier * seed + increment) % modulus;
         return seed;
     }
 
-    // Получение значения m
-    uint32_t getM() const {
-        return m;
+    uint32_t getModulus() const {
+        return modulus;
     }
 };
 
-// Функция для проведения графического теста
-void graphicalTest(std::vector<double>& data) {
-    // Гистограмма для оценки равномерности распределения
+void visualizeDistribution(std::vector<double>& data) {
     const int numBins = 10;
     std::vector<int> histogram(numBins, 0);
-    double min = *min_element(data.begin(), data.end());
-    double max = *max_element(data.begin(), data.end());
+    double minVal = *min_element(data.begin(), data.end());
+    double maxVal = *max_element(data.begin(), data.end());
 
     for (double value : data) {
         int bin;
-        if (max != min) {
-            bin = static_cast<int>((value - min) / (max - min) * numBins);
+        if (maxVal != minVal) {
+            bin = static_cast<int>((value - minVal) / (maxVal - minVal) * numBins);
         }
         else {
-            bin = 0; // Если max и min равны, то bin 0
-        }
-        if (bin < 0) {
             bin = 0;
-        }
-        else if (bin >= numBins) {
-            bin = numBins - 1;
-        }
-
-        histogram[bin]++; // Все эти проверки нужны для обработки исключения улетания в 0
-    }
-
-    // Вывод гистограммы
-    std::cout << "Гистограмма равномерности распределения:" << std::endl;
-    int maxCount = *max_element(histogram.begin(), histogram.end());
-    for (int i = 0; i < numBins; i++) {
-        double binStart = min + i * (max - min) / numBins;
-        double binEnd = min + (i + 1) * (max - min) / numBins;
-
-        std::cout << "[" << std::fixed << std::setprecision(2) << binStart << ", " << binEnd << "]: ";
-
-        int barLength = static_cast<int>(static_cast<double>(histogram[i]) / maxCount * 40); // Ширина столбца
-        for (int j = 0; j < barLength; j++) {
-            std::cout << "*";
-        }
-
-        std::cout << " " << histogram[i] << std::endl;
-    }
-}
-
-
-void chiSquareTest(std::vector<double>& data, int numBins) {
-    // Гистограмма для оценки равномерности распределения
-    std::vector<int> histogram(numBins, 0);
-    double min = *min_element(data.begin(), data.end());
-    double max = *max_element(data.begin(), data.end());
-
-    for (double value : data) {
-        int bin;
-        if (max != min) {
-            bin = static_cast<int>((value - min) / (max - min) * numBins);
-        }
-        else {
-            bin = 0; // Если max и min равны, то bin 0
         }
         if (bin < 0) {
             bin = 0;
@@ -96,35 +48,73 @@ void chiSquareTest(std::vector<double>& data, int numBins) {
         histogram[bin]++;
     }
 
-    // Статистическое тестирование (тест хи-квадрат)
-    double chiSquared = 0.0;
-    double expectedFrequency = static_cast<double>(data.size()) / numBins;
+    std::cout << "Гистограмма распределения:" << std::endl;
+    int maxCount = *max_element(histogram.begin(), histogram.end());
     for (int i = 0; i < numBins; i++) {
-        chiSquared += pow(histogram[i] - expectedFrequency, 2) / expectedFrequency;
-    }
+        double binStart = minVal + i * (maxVal - minVal) / numBins;
+        double binEnd = minVal + (i + 1) * (maxVal - minVal) / numBins;
 
-    int degreesOfFreedom = numBins - 1; // Степени свободы (число интервалов - 1)
-    double significanceLevel = 0.05; // Уровень значимости
-    double chiSquaredTable(int degreesOfFreedom, double significanceLevel);
+        std::cout << "[" << std::fixed << std::setprecision(2) << binStart << ", " << binEnd << "]: ";
 
-    // Вычисление критического значения из таблицы хи-квадрат для заданного уровня значимости и степеней свободы
-    double criticalValue = chiSquaredTable(degreesOfFreedom, significanceLevel);
+        int barLength = static_cast<int>(static_cast<double>(histogram[i]) / maxCount * 40);
+        for (int j = 0; j < barLength; j++) {
+            std::cout << "*";
+        }
 
-    std::cout << "Статистический тест хи-квадрат:" << std::endl;
-    std::cout << "Значение хи-квадрат: " << chiSquared << std::endl;
-    std::cout << "Степени свободы: " << degreesOfFreedom << std::endl;
-    std::cout << "Критическое значение: " << criticalValue << std::endl;
-
-    if (chiSquared <= criticalValue) {
-        std::cout << "Тест пройден. Распределение является равномерным." << std::endl;
-    }
-    else {
-        std::cout << "Тест не пройден. Распределение не является равномерным." << std::endl;
+        std::cout << " " << histogram[i] << std::endl;
     }
 }
 
-double chiSquaredTable(int degreesOfFreedom, double significanceLevel) {
-    if (degreesOfFreedom >= 1 && significanceLevel == 0.05) { // Таблицу взял из интернета, степеней свободы при данных условиях 9
+void chiSquareStatisticalTest(std::vector<double>& data, int numBins) {
+    std::vector<int> histogram(numBins, 0);
+    double minVal = *min_element(data.begin(), data.end());
+    double maxVal = *max_element(data.begin(), data.end());
+
+    for (double value : data) {
+        int bin;
+        if (maxVal != minVal) {
+            bin = static_cast<int>((value - minVal) / (maxVal - minVal) * numBins);
+        }
+        else {
+            bin = 0;
+        }
+        if (bin < 0) {
+            bin = 0;
+        }
+        else if (bin >= numBins) {
+            bin = numBins - 1;
+        }
+
+        histogram[bin]++;
+    }
+
+    double chiSquaredValue = 0.0;
+    double expectedFrequency = static_cast<double>(data.size()) / numBins;
+    for (int i = 0; i < numBins; i++) {
+        chiSquaredValue += pow(histogram[i] - expectedFrequency, 2) / expectedFrequency;
+    }
+
+    int degreesOfFreedom = numBins - 1;
+    double significanceLevel = 0.05;
+    double lookupChiSquared(int degreesOfFreedom, double significanceLevel);
+
+    double criticalValue = lookupChiSquared(degreesOfFreedom, significanceLevel);
+
+    std::cout << "Статистический тест хи-квадрат:" << std::endl;
+    std::cout << "Значение хи-квадрат: " << chiSquaredValue << std::endl;
+    std::cout << "Степени свободы: " << degreesOfFreedom << std::endl;
+    std::cout << "Критическое значение: " << criticalValue << std::endl;
+
+    if (chiSquaredValue <= criticalValue) {
+        std::cout << "Тест пройден. Распределение равномерное." << std::endl;
+    }
+    else {
+        std::cout << "Тест не пройден. Распределение не равномерное." << std::endl;
+    }
+}
+
+double lookupChiSquared(int degreesOfFreedom, double significanceLevel) {
+    if (degreesOfFreedom >= 1 && significanceLevel == 0.05) {
         switch (degreesOfFreedom) {
         case 1: return 3.841;
         case 2: return 5.991;
@@ -135,7 +125,7 @@ double chiSquaredTable(int degreesOfFreedom, double significanceLevel) {
         case 7: return 14.067;
         case 8: return 15.507;
         case 9: return 16.919;
-        default: return 0.0; 
+        default: return 0.0;
         }
     }
     else {
@@ -143,47 +133,37 @@ double chiSquaredTable(int degreesOfFreedom, double significanceLevel) {
     }
 }
 
-
 int main() {
     setlocale(LC_ALL, "Russian");
     const int numSamples = 10000;
     std::vector<double> generatedValues;
 
-    // Инициализация Линейного конгруэнтного генератора
-    LinearCongruentialGenerator lcg(12345);
+    RandomNumberGenerator rng(12345);
 
-    // Генерация случайных чисел
     for (int i = 0; i < numSamples; i++) {
-        double randomValue = static_cast<double>(lcg.generate()) / static_cast<double>(lcg.getM());
+        double randomValue = static_cast<double>(rng.generate()) / static_cast<double>(rng.getModulus());
         generatedValues.push_back(randomValue);
     }
 
-    std::cout << "Тестирование Линейного конгруэнтного генератора:" << std::endl;
-    // Графический тест
-    graphicalTest(generatedValues);
+    std::cout << "Тестирование генератора случайных чисел:" << std::endl;
+    visualizeDistribution(generatedValues);
 
-    // Проведение теста хи-квадрат
-    int numBins = 10; // Количество интервалов в гистограмме
-    chiSquareTest(generatedValues, numBins);
+    int numBins = 10;
+    chiSquareStatisticalTest(generatedValues, numBins);
 
-    // Используем библиотечный генератор
     std::random_device rd;
     std::mt19937 mt(rd());
     std::uniform_real_distribution<double> dist(0.0, 1.0);
     std::vector<double> libraryGeneratedValues;
 
-    // Генерация случайных чисел с использованием библиотечного генератора
     for (int i = 0; i < numSamples; i++) {
         double randomValue = dist(mt);
         libraryGeneratedValues.push_back(randomValue);
     }
 
-    std::cout << "Тестирование библиотечного генератора:" << std::endl;
-    // Графический тест для библиотечного генератора
-    graphicalTest(libraryGeneratedValues);
-
-    // Проведение теста хи-квадрат для библиотечного генератора
-    chiSquareTest(libraryGeneratedValues, numBins);
+    std::cout << "Тестирование библиотечного генератора случайных чисел:" << std::endl;
+    visualizeDistribution(libraryGeneratedValues);
+    chiSquareStatisticalTest(libraryGeneratedValues, numBins);
 
     return 0;
 }
